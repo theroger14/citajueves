da1206d7b678a492a62fa88e9e9771b182221030
theroger14 <156637309+theroger14@users.noreply.github.com>
2024-04-24 12:42:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si. Te quiero mucho itzel bonita')
+    alert('Sabia que ibas a decir que si. Te quiero mucho')
 });
 
 const noBtn = document.querySelector('#noBtn');
